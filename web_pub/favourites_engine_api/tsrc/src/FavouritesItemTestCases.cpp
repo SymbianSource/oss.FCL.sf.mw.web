c8caae525e42e9d325b5e5c976737291c343cb28
Mikko Sunikka <mikko.sunikka@nokia.com>
2009-11-06 13:15:00 +0200

    Revision: 200941 Kit: 200945

--- web_pub/favourites_engine_api/tsrc/src/FavouritesItemTestCases.cpp
+++ web_pub/favourites_engine_api/tsrc/src/FavouritesItemTestCases.cpp
@@ -1,24 +1,21 @@
 /*
-* ============================================================================
-*  Name:      FavouritesItemTestCases.cpp
-*  Part of:   FavouritesEngineTest class member functions   
+* Copyright (c) 2008 Nokia Corporation and/or its subsidiary(-ies).
+* All rights reserved.
+* This component and the accompanying materials are made available
+* under the terms of the License "Eclipse Public License v1.0"
+* which accompanies this distribution, and is available
+* at the URL "http://www.eclipse.org/legal/epl-v10.html".
 *
-*  Description:
+* Initial Contributors:
+* Nokia Corporation - initial contribution.
 *
-*  Version:   1.0
+* Contributors:
 *
-*  Copyright (C) 2008 Nokia Corporation.
-*  This material, including documentation and any related 
-*  computer programs, is protected by copyright controlled by 
-*  Nokia Corporation. All rights are reserved. Copying, 
-*  including reproducing, storing,  adapting or translating, any 
-*  or all of this material requires the prior written consent of 
-*  Nokia Corporation. This material also contains confidential 
-*  information which may not be disclosed to others without the 
-*  prior written consent of Nokia Corporation.
+* Description: 
 *
-* ============================================================================
+*
 */
+
 
 
 // INCLUDE FILES
