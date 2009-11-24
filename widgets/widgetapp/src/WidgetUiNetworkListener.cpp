a50f28df62fcc67065011e25b4531054eb8bf110
Dremov Kirill (Nokia-D-MSW/Tampere) <kirill.dremov@nokia.com>
2009-11-24 09:02:20 +0200

    Revision: 200941 Kit: 200948

--- widgets/widgetapp/src/WidgetUiNetworkListener.cpp
+++ widgets/widgetapp/src/WidgetUiNetworkListener.cpp
@@ -1,20 +1,22 @@
-/*
-* Copyright (c) 2008, 2009 Nokia Corporation and/or its subsidiary(-ies).
-* All rights reserved.
-* This component and the accompanying materials are made available
-* under the terms of the License "Eclipse Public License v1.0"
-* which accompanies this distribution, and is available
-* at the URL "http://www.eclipse.org/legal/epl-v10.html".
-*
-* Initial Contributors:
-* Nokia Corporation - initial contribution.
-*
-* Contributors:
-*
-* Description: 
-*
-*/
-
+//
+//==============================================================================
+// Name        : WidgetUiNetworkListener.cpp
+// Part of     : WidgetUI
+// Interface   :
+// Description :
+// Version      : %version: 5 %
+//
+// Copyright  2008, 2009 Nokia Corporation.
+// This material, including documentation and any related
+// computer programs, is protected by copyright controlled by
+// Nokia Corporation. All rights are reserved. Copying,
+// including reproducing, storing, adapting or translating, any
+// or all of this material requires the prior written consent of
+// Nokia Corporation. This material also contains confidential
+// information which may not be disclosed to others without the
+// prior written consent of Nokia Corporation.
+//==============================================================================
+//
 
 #include "WidgetUiNetworkListener.h"
 
